0b90639c94ae570552ff88553b80ba6de41a698d
Hogan <hd089@uowmail.edu.au>
2024-09-22 15:09:30 +1000

    Update ride booking logic

--- ride_app/project/objects.py
+++ ride_app/project/objects.py
@@ -1,4 +1,13 @@
 from . import db
+from enum import Enum
+
+class OrderEnum(Enum):
+    INIT = 1
+    PAID = 2
+    ONBOARD = 3
+    COMPLETE = 4
+    CANCEL = 5
+    INVAILD = 6
 
 class Order(db.Model):
     # use to json serialization 
@@ -6,6 +15,18 @@ class Order(db.Model):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
     
     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    user_id = db.Column(db.Integer)
     name = db.Column(db.String(1000))
     price = db.Column(db.Integer)
+    state = db.Column(db.Integer)
+    datetime = db.Column(db.Integer)
     
+class OrderBill(db.Model):
+    # use to json serialization 
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+    
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    order_id = db.Column(db.Integer)
+    datetime = db.Column(db.Integer)
+    has_pay = db.Column(db.Boolean)
