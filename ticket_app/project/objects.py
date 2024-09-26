70274a74155071b32bfd7f88ac33ea8afe89608c
Hogan <hd089@uowmail.edu.au>
2024-09-26 17:27:31 +1000

    Tick App add static page

--- ticket_app/project/objects.py
+++ ticket_app/project/objects.py
@@ -9,3 +9,35 @@ class Order(db.Model):
     name = db.Column(db.String(1000))
     price = db.Column(db.Integer)
     
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    date = db.Column(db.String(50), nullable=False)
+    image = db.Column(db.String(200))
+    additional_info = db.Column(db.Text, nullable=True)
+
+
+    # ticketings = db.relationship('Ticketing', backref='event', lazy=True)
+    # updates = db.relationship('EventUpdate', backref='event', lazy=True)
+
+class Ticketing(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    event_id = db.Column(db.Integer, db.ForeignKey('event.id'), nullable=False)
+    date = db.Column(db.String(50), nullable=False)
+    time_slot = db.Column(db.String(20), nullable=False)
+    available_tickets = db.Column(db.Integer, nullable=False)
+    booked_quantity = db.Column(db.Integer, nullable=True, default=0)
+    email = db.Column(db.String(100), nullable=True)  
+
+
+class EventUpdate(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    event_id = db.Column(db.Integer, db.ForeignKey('event.id'), nullable=False)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    date = db.Column(db.String(50), nullable=False)
+    source = db.Column(db.String(100))
+    image = db.Column(db.String(200))
+    read_time = db.Column(db.String(20))
+    type = db.Column(db.String(20), nullable=False)  
